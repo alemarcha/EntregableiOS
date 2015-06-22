d16a867bec4d0d19a8dabdaaadb4e02154147146
Alexis Martínez <alemarcha26@gmail.com>
2015-06-22 21:28:15 +0200

    Initial commit

---
+++ EntregableAlexis/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EntregableAlexis
+//
+//  Created by Alexis Martínez on 22/6/15.
+//  Copyright (c) 2015 Alexis Martínez Chacón. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EntregableAlexis/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EntregableAlexis
+//
+//  Created by Alexis Martínez on 22/6/15.
+//  Copyright (c) 2015 Alexis Martínez Chacón. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
