eb509dd9ab8658756d3c915c8f4cfe59984b398b
Alexis Martínez <alemarcha26@gmail.com>
2015-06-23 15:29:02 +0200

    Añadido eventos al loginViewController

---
+++ EntregableAlexis/LoginViewController.h
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.h
+//  EntregableAlexis
+//
+//  Created by Alexis Martínez on 23/6/15.
+//  Copyright (c) 2015 Alexis Martínez Chacón. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIButton *loginButton;
+- (IBAction)onClickLoginButton:(id)sender;
+
+@end
